288a5e48ae59cc6a5bd0e49fc775d2f5a898dd4b
Erdemsonmezz <168731748+Erdemsonmezz@users.noreply.github.com>
2024-05-05 17:12:32 +0300

    Ikıncı commıt

--- testn.c
+++ testn.c
@@ -2,18 +2,7 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int bol(int a, int b) {
-	
-	if (b <= 0) {
-		return -1;
-	} else if (a > b) {
-		return -2;
-	} else {
-		
-	return a/b;
-	}
-	
-}
+int bol(int a, int b)
 
 int main(int argc, char *argv[]) {
 	int sonuc;
