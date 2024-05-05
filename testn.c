335ff1e298df7371a5bc9760c3d18dec974c398b
Erdemsonmezz <168731748+Erdemsonmezz@users.noreply.github.com>
2024-05-05 17:07:57 +0300

    Bolme fonksiyonu tanimlandi.

---
+++ testn.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int bol(int a, int b) {
+	
+	if (b <= 0) {
+		return -1;
+	} else if (a > b) {
+		return -2;
+	} else {
+		
+	return a/b;
+	}
+	
+}
+
+int main(int argc, char *argv[]) {
+	int sonuc;
+	
+	sonuc = bol(4,0);
+	
+	
+	printf("%d", sonuc);
+	
+	
+	return 0;
+}
